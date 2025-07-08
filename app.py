2b6672ae85472f0cd2492c1691d76114ad96def6
andreakollova <andrea_kollova@hotmail.com>
2025-07-08 21:02:20 +0200

    kniznica

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = '<your_uri>'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uwpbrzasi4dzqlld8ekl:B7ADHjpsKnVUWKu71i9aBDA7oOlyEY@bbqhgph7wb0iza3ajqzn-postgresql.services.clever-cloud.com:50013/bbqhgph7wb0iza3ajqzn'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -19,10 +20,29 @@ def authors():
         authors_dict.append(author.dict())
     return jsonify(authors_dict)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/authors/<int:id>')
 def author(id):
     author = Author.query.get(id)
     return jsonify(author.dict())
+
+@app.route('/authors-table')
+def authors_table():
+    authors = Author.query.all()
+    return render_template('authors.html', authors=authors)
+
+@app.route('/books-table')
+def books_table():
+    books = Book.query.all()
+    return render_template('books.html', books=books)
+
+@app.route('/genres-table')
+def genres_table():
+    genres = Genre.query.all()
+    return render_template('genres.html', genres=genres)
 
 @app.route('/authors-by-name/<name>')
 def author_by_name(name):
